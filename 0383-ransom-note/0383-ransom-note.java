ee5ae5f83ee4170e1936195ce91a50923fe24f14
Darcy McCusker <110570447+demccusker@users.noreply.github.com>
2024-11-07 18:23:48 -0800

    Time: 16 ms (18.7%), Space: 45.1 MB (31.44%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        boolean canConstruct = true;
+        Map<Character, Integer> noteCounts = new HashMap<>();
+        Map<Character, Integer> magazineCounts = new HashMap<>();
+        for (int i = 0; i < ransomNote.length(); i++){
+            noteCounts.put(ransomNote.charAt(i), noteCounts.getOrDefault(ransomNote.charAt(i), 0) + 1);
+        }
+        for (int j = 0; j < magazine.length(); j++){
+            magazineCounts.put(magazine.charAt(j), magazineCounts.getOrDefault(magazine.charAt(j), 0) + 1);
+        }
+        for (Character c : noteCounts.keySet()){
+            if (magazineCounts.getOrDefault(c,0) < noteCounts.get(c)){
+                canConstruct = false;
+            }
+        }
+        return canConstruct;
+    }
+}
